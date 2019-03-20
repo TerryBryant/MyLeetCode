00d94b7146364673ee848722d4b51b0a389aabca
Terry <dageda1991@163.com>
2019-03-20 21:49:11 +0800

    Update sort.cpp

--- sort.cpp
+++ sort.cpp
@@ -57,6 +57,56 @@ void hillSort(vector<int>& nums){
     ;
 }
 
+void merge(int* nums, int left, int mid, int right, int* temp){
+    int i = left;
+    int j = mid + 1;
+    int t = 0;
+
+    while (i<=mid && j<=right){
+        if(nums[i] <= nums[j])
+            temp[t++] = nums[i++];
+        else
+            temp[t++] = nums[j++];
+    }
+
+    while (i<=mid){
+        temp[t++] = nums[i++];
+    }
+
+    while (j<=right){
+        temp[t++] = nums[j++];
+    }
+
+    t = 0;
+    while (left <= right)
+        nums[left++] = temp[t++];
+}
+
+void mergeSort(int* nums, int left, int right, int* temp){
+    if(left < right){
+        int mid = (left + right) / 2;
+        sort(nums, left, mid, temp);
+        sort(nums, mid + 1, right, temp);
+        merge(nums, left, mid, right, temp);
+    }
+}
+
+
+void mergeSort(int* nums, int length){
+    int* temp = new int[length];
+    sort(nums, 0, length - 1, temp);
+}
+
+
+int main(){
+    int nums[] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+    sort(nums, sizeof(nums) / sizeof(int));
+
+    for(int num: nums) cout<<num<<" ";
+
+    return 1;
+}
+
 int main() {
     vector<int> nums = {34, 2, 4, 567, 18, 999, 450};
 
